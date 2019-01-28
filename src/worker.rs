fbf01f581b4bbb2eee88cecfb2a8043d122aba3d
Nathan Kent <nate@nkent.net>
2019-01-27 22:40:58 -0500

    Implement worker node

---
+++ src/worker.rs
@@ -0,0 +1,46 @@
+//! Worker nodes and utilities.
+use failure::{Error, ResultExt};
+use nng::{Socket, Protocol};
+use log::{info, debug};
+
+/// Creates a worker node that executes the callback function upon receiving work.
+///
+/// This function requires a callback that will be run every time the worker node receives work. The
+/// raw message is passed to the callback function and the returned data is sent directly back to
+/// the requester. If multiple URLS are specified, the worker will split its work fairly between all
+/// connected requesters.
+///
+/// This function is largely just a wrapper around an NNG "Reply" socket, which means that the
+/// requester does not necessarily have to be a Coordinator node.
+pub fn start<'a, U, C>(urls: U, mut callback: C) -> Result<(), Error>
+	where U: IntoIterator<Item = &'a str>,
+	      C: FnMut(&[u8]) -> Result<Vec<u8>, Error>
+{
+	info!("Opening NNG socket");
+	let mut socket = Socket::new(Protocol::Rep0).context("Unable to open socket")?;
+
+	// Setting the socket to non-blocking mode for the dial operations will allow us to start the
+	// workers before the coordinator.
+	socket.set_nonblocking(true);
+	urls.into_iter()
+		.inspect(|url| info!("Dialing to {}", url))
+		.map(|url| socket.dial(url).context("Unable to dial to URL"))
+		.collect::<Result<_, _>>()?;
+
+	// Now that we've dialed out, go back to blocking mode to make managing the work easier.
+	socket.set_nonblocking(false);
+
+	info!("Beginning work loop");
+	loop {
+		debug!("Waiting for work");
+		let work = socket.recv().context("Failed to receive work")?;
+
+		debug!("Entering callback function");
+		let result = callback(&work).context("Callback function resulted in an error")?;
+
+		debug!("Reply with result");
+		socket.send(result[..].into())
+			.map_err(|(_, e)| e)
+			.context("Failed to send results")?;
+	}
+}
